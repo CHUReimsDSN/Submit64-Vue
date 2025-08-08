178bfc1343d51ca460db40a5c6f311a74c224986
Jules Debeaumont <jdebeaumont@chu-reims.fr>
2025-08-08 14:23:49 +0200

    [FIX] fixing cast sym to string form beautify label

--- src/Submit64Form.vue
+++ src/Submit64Form.vue
@@ -105,9 +105,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  {{ setupIsDone }}
-  <pre>{{ generatedForm }}</pre>
-  <div v-if="setupIsDone && generatedForm" class="flex q-pa-sm q-gutter-sm">
+  <div v-if="setupIsDone && generatedForm" class="flex column q-pa-sm q-gutter-sm">
     <Component
       v-for="(section, indexSection) in generatedForm.sections"
       :key="indexSection"
